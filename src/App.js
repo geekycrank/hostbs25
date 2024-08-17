ae43192c4d062b5265d87e0935412db606269a8b
Barathkumar8610 <barathkumar8610@gmail.com>
2024-08-18 01:19:51 +0530

    Updation 7

--- src/App.js
+++ src/App.js
@@ -13,45 +13,41 @@ function App()
 
   const events=[
     {
-      eventName:"SHIPWRECK",
-      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      eventName:"ADZAP",
+      description:"Pitch the most outrageous product ideas imaginable! Say invisible socks or a smartphone that only works when you're asleep. Get creative, get funny, and keep the audience in stitches.",
       guidelines:[
-        'Each team should consist of 2 members.',
-        'The event will feature 2 rounds.'
+        'Your product idea/ad should be unique and humorous. Think outside the box.',
+        'Ensure all team members have a role in the ad and contribute to the overall performance.',
+        'The use of simple props is allowed. Make sure any props are easy to manage within the time constraints.',
+        'Be mindful of the time.'
       ],
-      rules:[
-        'Use of mobile phones during the event will result in disqualification.',
-        'The preliminaries will take place strictly within the allotted time.',
-        'The top 6 teams will advance to the finals.',
-        'Final round rules will be announced on the spot.'
+      prelims:[
+        'Each team should have 4-6 members.',
+        'You will have 7 minutes to brainstorm and script your ad.',
+        'Your performance should last 4-5 minutes. Going over the time limit will cost you points.',
+        'Make sure your humor is clean. Offensive content or vulgar language is not allowed.',
+        "The judge's decision is final."
       ]
     },
     {
-      eventName:"QUIZ",
-      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      eventName:"DEBATE",
+      description:"Prepare to engage in a battle of wits, where your critical thinking and communication skills will be put to the ultimate test. This event is designed to test your ability to argue both for and against a given topic, clearly and coherently, in a limited time setting.",
       guidelines:[
-        'Each team should consist of 2 members.',
-        'The event will feature 2 rounds.'
+        "Individual Participation.",
+        "Two-round event."
       ],
-      rules:[
-        'Use of mobile phones during the event will result in disqualification.',
-        'The preliminaries will take place strictly within the allotted time.',
-        'The top 6 teams will advance to the finals.',
-        'Final round rules will be announced on the spot.'
-      ]
-    },
-    {
-      eventName:"SHIPWRECK",
-      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
-      guidelines:[
-        'Each team should consist of 2 members.',
-        'The event will feature 2 rounds.'
+      prelims:[
+        'Each team should have 4-6 members.',
+        'You will have 7 minutes to brainstorm and script your ad.',
+        'Your performance should last 4-5 minutes. Going over the time limit will cost you points.',
+        'Make sure your humor is clean. Offensive content or vulgar language is not allowed.',
+        "The judge's decision is final."
       ],
-      rules:[
-        'Use of mobile phones during the event will result in disqualification.',
-        'The preliminaries will take place strictly within the allotted time.',
-        'The top 6 teams will advance to the finals.',
-        'Final round rules will be announced on the spot.'
+      finals:[
+        "Teams will be formed from those selected in Round 1.",
+        "Teams will be given a topic 10 minutes before the debate begins.",
+        "Speech Structure:- \nThe first two contestants from each team will present arguments in favor of the topic.\n- The third contestant will present a rebuttal speech, countering the points made by the opposing team.",
+        "The jury's decision will be final."
       ]
     },
     {
@@ -61,21 +57,7 @@ function App()
         'Each team should consist of 2 members.',
         'The event will feature 2 rounds.'
       ],
-      rules:[
-        'Use of mobile phones during the event will result in disqualification.',
-        'The preliminaries will take place strictly within the allotted time.',
-        'The top 6 teams will advance to the finals.',
-        'Final round rules will be announced on the spot.'
-      ]
-    },
-    {
-      eventName:"SHIPWRECK",
-      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
-      guidelines:[
-        'Each team should consist of 2 members.',
-        'The event will feature 2 rounds.'
-      ],
-      rules:[
+      prelims:[
         'Use of mobile phones during the event will result in disqualification.',
         'The preliminaries will take place strictly within the allotted time.',
         'The top 6 teams will advance to the finals.',
@@ -89,7 +71,7 @@ function App()
         'Each team should consist of 2 members.',
         'The event will feature 2 rounds.'
       ],
-      rules:[
+      prelims:[
         'Use of mobile phones during the event will result in disqualification.',
         'The preliminaries will take place strictly within the allotted time.',
         'The top 6 teams will advance to the finals.',
@@ -103,7 +85,35 @@ function App()
         'Each team should consist of 2 members.',
         'The event will feature 2 rounds.'
       ],
-      rules:[
+      prelims:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
+    },
+    {
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      prelims:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
+    },
+    {
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      prelims:[
         'Use of mobile phones during the event will result in disqualification.',
         'The preliminaries will take place strictly within the allotted time.',
         'The top 6 teams will advance to the finals.',
@@ -120,13 +130,13 @@ function App()
       
       <Route path="/Events" element={<Events></Events>} />
       <Route path="/Register" element={<Register></Register>} />
-      <Route path="/event1" element={<Event eventName={events[0].eventName} eventDescription={events[0].description} eventGuidelines={events[0].guidelines} eventRules={events[0].rules}></Event>} />
-      <Route path="/event2" element={<Event eventName={events[1].eventName} eventDescription={events[1].description} eventGuidelines={events[1].guidelines} eventRules={events[1].rules}></Event>} />
-      <Route path="/event3" element={<Event eventName={events[2].eventName} eventDescription={events[2].description} eventGuidelines={events[2].guidelines} eventRules={events[2].rules}></Event>} />
-      <Route path="/event5" element={<Event eventName={events[3].eventName} eventDescription={events[3].description} eventGuidelines={events[3].guidelines} eventRules={events[3].rules}></Event>} />
-      <Route path="/event6" element={<Event eventName={events[4].eventName} eventDescription={events[4].description} eventGuidelines={events[4].guidelines} eventRules={events[4].rules}></Event>} />
-      <Route path="/event7" element={<Event eventName={events[5].eventName} eventDescription={events[5].description} eventGuidelines={events[5].guidelines} eventRules={events[5].rules}></Event>} />
-      <Route path="/event8" element={<Event eventName={events[6].eventName} eventDescription={events[6].description} eventGuidelines={events[6].guidelines} eventRules={events[6].rules}></Event>} />
+      <Route path="/event1" element={<Event eventName={events[0].eventName} eventDescription={events[0].description} eventGuidelines={events[0].guidelines} eventRules={events[0].prelims} eventFinals={events[0].finals}></Event>} />
+      <Route path="/event2" element={<Event eventName={events[1].eventName} eventDescription={events[1].description} eventGuidelines={events[1].guidelines} eventRules={events[1].prelims} eventFinals={events[1].finals}></Event>} />
+      <Route path="/event3" element={<Event eventName={events[2].eventName} eventDescription={events[2].description} eventGuidelines={events[2].guidelines} eventRules={events[2].prelims} eventFinals={events[2].finals}></Event>} />
+      <Route path="/event5" element={<Event eventName={events[3].eventName} eventDescription={events[3].description} eventGuidelines={events[3].guidelines} eventRules={events[3].prelims} eventFinals={events[3].finals}></Event>} />
+      <Route path="/event6" element={<Event eventName={events[4].eventName} eventDescription={events[4].description} eventGuidelines={events[4].guidelines} eventRules={events[4].prelims} eventFinals={events[4].finals}></Event>} />
+      <Route path="/event7" element={<Event eventName={events[5].eventName} eventDescription={events[5].description} eventGuidelines={events[5].guidelines} eventRules={events[5].prelims} eventFinals={events[5].finals}></Event>} />
+      <Route path="/event8" element={<Event eventName={events[6].eventName} eventDescription={events[6].description} eventGuidelines={events[6].guidelines} eventRules={events[6].prelims} eventFinals={events[6].finals}></Event>} />
         
     </Routes>
     
--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -15,8 +15,8 @@ function Mobile()
     {
       link:"/event1",
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn-fgwmpjs1IWLX6Zk_b1Nmx9q51-tnsixC1n7SoiMbGYCoLUw",
-      title: "SHIPWRECK",
-      description: "A Tempest of wit and wisdom, where words and sense of humour are the life boats that save or sink, you're a crewmate on a sinking ship, and there's only one life jacket left. Your mission? Convince the captain to save you!"
+      title: " ADZAP",
+      description: "Pitch the most outrageous product ideas imaginable! Say invisible socks or a smartphone that only works when you're asleep. Get creative, get funny, and keep the audience in stitches."
     },
     {
       link:"/event2",  
