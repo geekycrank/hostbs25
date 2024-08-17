fb504274172e951ab68cf8c27576b045d117dd1b
Barathkumar8610 <barathkumar8610@gmail.com>
2024-08-18 00:24:50 +0530

    EventDescriptionPage Developed

--- src/App.js
+++ src/App.js
@@ -13,25 +13,102 @@ function App()
 
   const events=[
     {
-      eventName:"event1"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event2"
+      eventName:"QUIZ",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event3"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event4"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event5"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event6"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
     {
-      eventName:"event7"
+      eventName:"SHIPWRECK",
+      description:"Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!",
+      guidelines:[
+        'Each team should consist of 2 members.',
+        'The event will feature 2 rounds.'
+      ],
+      rules:[
+        'Use of mobile phones during the event will result in disqualification.',
+        'The preliminaries will take place strictly within the allotted time.',
+        'The top 6 teams will advance to the finals.',
+        'Final round rules will be announced on the spot.'
+      ]
     },
   ]
  
@@ -43,13 +120,13 @@ function App()
       
       <Route path="/Events" element={<Events></Events>} />
       <Route path="/Register" element={<Register></Register>} />
-      <Route path="/event1" element={<Event eventName={events[0].eventName}></Event>} />
-      <Route path="/event2" element={<Event eventName={events[1].eventName}></Event>} />
-      <Route path="/event3" element={<Event eventName={events[2].eventName}></Event>} />
-      <Route path="/event5" element={<Event eventName={events[3].eventName}></Event>} />
-      <Route path="/event6" element={<Event eventName={events[4].eventName}></Event>} />
-      <Route path="/event7" element={<Event eventName={events[5].eventName}></Event>} />
-      <Route path="/event8" element={<Event eventName={events[6].eventName}></Event>} />
+      <Route path="/event1" element={<Event eventName={events[0].eventName} eventDescription={events[0].description} eventGuidelines={events[0].guidelines} eventRules={events[0].rules}></Event>} />
+      <Route path="/event2" element={<Event eventName={events[1].eventName} eventDescription={events[1].description} eventGuidelines={events[1].guidelines} eventRules={events[1].rules}></Event>} />
+      <Route path="/event3" element={<Event eventName={events[2].eventName} eventDescription={events[2].description} eventGuidelines={events[2].guidelines} eventRules={events[2].rules}></Event>} />
+      <Route path="/event5" element={<Event eventName={events[3].eventName} eventDescription={events[3].description} eventGuidelines={events[3].guidelines} eventRules={events[3].rules}></Event>} />
+      <Route path="/event6" element={<Event eventName={events[4].eventName} eventDescription={events[4].description} eventGuidelines={events[4].guidelines} eventRules={events[4].rules}></Event>} />
+      <Route path="/event7" element={<Event eventName={events[5].eventName} eventDescription={events[5].description} eventGuidelines={events[5].guidelines} eventRules={events[5].rules}></Event>} />
+      <Route path="/event8" element={<Event eventName={events[6].eventName} eventDescription={events[6].description} eventGuidelines={events[6].guidelines} eventRules={events[6].rules}></Event>} />
         
     </Routes>
     
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,53 @@
 import React from "react";
+import "./Event.css";
 
-function Event(props){
+function Event(props) {
+  return (
+    <div className="Event">
+       <div className="outerBox">
+           <div className="innerBox">
+              <div className="eventName">
+                  {props.eventName}
+              </div>
 
-    return <div>
-        <h1>{props.eventName}</h1>
+              <div className="eventDescription">
+                   <div className="descHeading">Description</div>
+                   <div className="descContent">
+                        Get ready for a fun quiz that tests your knowledge and quick thinking. Compete with the best and aim to be the top quiz champion. Only the sharpest will win. Show your skills and rise to the challenge!
+                   </div>
+              </div>
+
+              <div className="guidelines">
+                  <div className="guideHeading">Guidelines</div>
+                  <ol className="guideContent">
+                     {props.eventGuidelines.map((guideline, index) => (
+                     <li key={index}>{guideline}</li>
+                      ))}
+                  </ol> 
+              </div>
+
+              <div className="rules">
+                  <div className="rulesHeading">Rules</div>
+                  <ol className="rulesContent">
+                     {props.eventRules.map((guideline, index) => (
+                     <li key={index}>{guideline}</li>
+                      ))}
+                  </ol> 
+              </div>
+           </div>
+       </div>
     </div>
-
+  );
 }
 
 export default Event;
+
+
+
+{/* <h1>{props.eventName}</h1>
+      <h2>{props.eventDescription}</h2>
+      <ol>
+        {props.eventGuidelines.map((guideline, index) => (
+          <li key={index}>{guideline}</li>
+        ))}
+      </ol> */}
